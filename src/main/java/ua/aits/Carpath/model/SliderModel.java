0b87385be0f449eb682c74c7a7077f48e46d51a6
kiwi <kiwi@127.0.53.53>
2015-07-10 12:59:21 +0300

    slider languages

--- src/main/java/ua/aits/Carpath/model/SliderModel.java
+++ src/main/java/ua/aits/Carpath/model/SliderModel.java
@@ -18,8 +18,16 @@ import ua.aits.Carpath.functions.DB;
 public class SliderModel {
     public Integer id;
     public String image;
-    public String url;
-    public String text;
+    public String urlEN;
+    public String urlUA;
+    public String urlHU;
+    public String urlSK;
+    public String urlRO;
+    public String textEN;
+    public String textUA;
+    public String textHU;
+    public String textSK;
+    public String textRO;
     
     public Integer getId() {
         return id;
@@ -35,36 +43,119 @@ public class SliderModel {
         this.image = image;
     }
     
-    public String getUrl() {
-        return url;
+    public String getUrlEN() {
+        return urlEN;
     }
-    public void setUrl(String url) {
-        this.url = url;
-    }
-    
-    public String getText() {
-        return text;
-    }
-    public void setText(String text) {
-        this.text = text;
+    public void setUrlEN(String urlEN) {
+        this.urlEN = urlEN;
     }
     
-    public List<SliderModel> getAllSlides() throws SQLException, ClassNotFoundException, InstantiationException, IllegalAccessException {
+    public String getUrlUA() {
+        return urlUA;
+    }
+    public void setUrlUA(String urlUA) {
+        this.urlUA = urlUA;
+    }
+    
+    public String getUrlHU() {
+        return urlHU;
+    }
+    public void setUrlHU(String urlHU) {
+        this.urlHU = urlHU;
+    }
+    
+    public String getUrlSK() {
+        return urlSK;
+    }
+    public void setUrlSK(String urlSK) {
+        this.urlSK = urlSK;
+    }
+    
+    public String getUrlRO() {
+        return urlRO;
+    }
+    public void setUrlRO(String urlRO) {
+        this.urlRO = urlRO;
+    }
+    
+    public String getTextEN() {
+        return textEN;
+    }
+    public void setTextEN(String textEN) {
+        this.textEN = textEN;
+    }
+    
+    public String getTextUA() {
+        return textUA;
+    }
+    public void setTextUA(String textUA) {
+        this.textUA = textUA;
+    }
+    
+    public String getTextHU() {
+        return textHU;
+    }
+    public void setTextHU(String textHU) {
+        this.textHU = textHU;
+    }
+    
+    public String getTextSK() {
+        return textSK;
+    }
+    public void setTextSK(String textSK) {
+        this.textSK = textSK;
+    }
+    
+    public String getTextRO() {
+        return textRO;
+    }
+    public void setTextRO(String textRO) {
+        this.textRO = textRO;
+    }
+    
+    public List<SliderModel> getAllSlides(String lan) throws SQLException, ClassNotFoundException, InstantiationException, IllegalAccessException {
         ResultSet result = DB.getResultSet("select * from slider order by id asc;");
         List<SliderModel> resultList = new LinkedList<>();
         while (result.next()) {
             SliderModel temp = new SliderModel();
             temp.setId(result.getInt("id"));
             temp.setImage(result.getString("image"));
-            temp.setUrl(result.getString("url"));
-            temp.setText(result.getString("text"));
+            temp.setUrlEN(result.getString("url"+lan.toUpperCase()));
+            temp.setTextEN(result.getString("text"+lan.toUpperCase()));
             resultList.add(temp);
         } 
         return resultList;
     }
     
-    public void insertSlide(String image, String url, String text)throws SQLException, ClassNotFoundException, InstantiationException, IllegalAccessException {
-        DB.runQuery("INSERT INTO `slider`(`image`, `url`, `text`) VALUES ('"+image+"','"+url+"','"+text+"');");
+    public List<SliderModel> getAllSlidesForEdit() throws SQLException, ClassNotFoundException, InstantiationException, IllegalAccessException {
+        ResultSet result = DB.getResultSet("select * from slider order by id asc;");
+        List<SliderModel> resultList = new LinkedList<>();
+        while (result.next()) {
+            SliderModel temp = new SliderModel();
+            temp.setId(result.getInt("id"));
+            temp.setImage(result.getString("image"));
+            temp.setUrlEN(result.getString("urlEN"));
+            temp.setUrlUA(result.getString("urlUA"));
+            temp.setUrlHU(result.getString("urlHU"));
+            temp.setUrlSK(result.getString("urlSK"));
+            temp.setUrlRO(result.getString("urlRO"));
+            temp.setTextEN(result.getString("textEN"));
+            temp.setTextUA(result.getString("textUA"));
+            temp.setTextHU(result.getString("textHU"));
+            temp.setTextSK(result.getString("textSK"));
+            temp.setTextRO(result.getString("textRO"));
+            resultList.add(temp);
+        } 
+        return resultList;
+    }
+    
+    public void insertSlide(String image, 
+            String urlEN, String urlUA, String urlHU, String urlSK, String urlRO, 
+            String textEN, String textUA, String textHU, String textSK, String textRO)throws SQLException, ClassNotFoundException, InstantiationException, IllegalAccessException {
+        DB.runQuery("INSERT INTO `slider`(`image`, `urlEN`, `urlUA`, `urlHU`, `urlSK`, `urlRO`,"
+                + " `textEN`, `textUA`, `textHU`, `textSK`, `textRO`) VALUES ("
+                + "'"+image+"','"+urlEN+"','"+urlUA+"','"+urlHU+"','"+urlSK+"','"+urlRO+"',"
+                + "'"+textEN+"','"+textUA+"','"+textHU+"','"+textSK+"','"+textRO+"');");
     }
     
     public void clearTable()throws SQLException, ClassNotFoundException, InstantiationException, IllegalAccessException {
